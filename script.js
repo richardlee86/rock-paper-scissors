6de7b2bb90521bbe34e29f97c9e304ce1f836664
richardlee86 <richard.t.lee86@gmail.com>
2022-01-31 16:36:32 +0100

    got the game logic working with the event listener for the buttons. Need to work on the code to play 5 rounds and keep score correctly

--- script.js
+++ script.js
@@ -16,11 +16,10 @@ const computerPlay = () => {
 }
 
 //Create function to play a single round of Rock Paper Scissors
-const playRound = (playerSelection, computerSelection) => {
-
-    //make playerSelection case-insensitive
-    playerSelection = playerSelection.toLowerCase(); 
+const playRound = (playerSelection, computerSelection) => {   
     
+    computerSelection = computerPlay();  
+     
 
     //conditional to return string based on results of game
     if (playerSelection === computerSelection){
@@ -36,24 +35,33 @@ const playRound = (playerSelection, computerSelection) => {
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
         return 'You Lose! Rock beats Scissors!'; 
     } else {
+        console.log(playerSelection);
+        console.log(computerSelection);
         return 'You Win! Scissors cuts Paper!';
     }
 
 }
 //Write a function called game that simulates the game being played 5 times and keeps score
-const game = () => {
+const game = (playerSelection, computerSelection) => {
+    this.computerSelection = computerPlay();
     let playerScore = 0;
     let computerScore = 0;
     let value;       
          
-        value = (playRound(prompt('Please choose Rock, Paper or Scissors'), computerPlay()));                
-        console.log(value);
-        if (value === 'You Win! Rock beats Scissors!' || value === 'You Win! Paper covers Rock!' ||
-                value === 'You Win! Scissors cuts Paper!'){
-                    playerScore++;
-                } else if(value === 'You Lose! Paper covers Rock!' || value === 'You Lose! Scissors cuts Paper!' || value === 'You Lose! Rock beats Scissors!') {
-                    computerScore++
-                }        
+        value = (playRound(playerSelection, computerSelection)); 
+        console.log(value);      
+                
+                
+                    if (value === 'You Win! Rock beats Scissors!' || value === 'You Win! Paper covers Rock!' ||
+                    value === 'You Win! Scissors cuts Paper!'){
+                        playerScore++;
+                    } else if(value === 'You Lose! Paper covers Rock!' || value === 'You Lose! Scissors cuts Paper!' || value === 'You Lose! Rock beats Scissors!') {
+                        computerScore++
+                    }                     
+                   
+                
+                    
+                
      
 
     //Display message based on scores of game 
@@ -69,23 +77,33 @@ const game = () => {
     }
 }
 
-let buttons = document.querySelectorAll('button');
+//QuerySelector to capture all three buttons
+const buttons = document.querySelectorAll('button');
+
+//Loop through buttons and apply an eventlistener to each that runs the game when clicked
+buttons.forEach(button => {
+    button.addEventListener('click', e => {
+        let playerSelection = button.innerHTML.toLowerCase();         
+      console.log(game(playerSelection));
+    })
+})
 
 //Need to add function to get playerSelection
-buttons.forEach(e =>{
-    e.addEventListener('click', playRound(playerSelection, computerPlay()));
-});
 
-function playerSelection() {
-buttons.forEach(e => {
-    e.addEventListener('click', function(){
-        if(e.innerHTML === 'Rock'){
-            return 'Rock';
-        } else if(e.innerHTML === 'Paper') {
-            return 'Paper'; 
-        } else if (e.innerHTML === 'Scissors'){
-            return 'Scissors'; 
-        }
-    })
-});
-}
+
+// buttons.addEventListener('click', function(e){
+//         if(e.innerHTML === 'Rock'){
+//             return 'Rock';
+//         } else if(e.innerHTML === 'Paper') {
+//             return 'Paper'; 
+//         } else if (e.innerHTML === 'Scissors'){
+//             return 'Scissors'; 
+//         }
+//     });
+
+// rock.addEventListener('click', e => {
+//     console.log('rock');
+// });
+
+
+
